bd612f0583b2524aef40b4da4f5b5cb414650d66
ALOK RANJAN SINGH <aloksingh29565@gmail.com>
2022-06-29 23:07:52 +0530

    initial commit

---
+++ src/pages/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Navigate,Outlet, Routes } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const PrivateRoute = () => {
+  const {isAuthenticated,user}=useAuth0();
+  const isUser = isAuthenticated && user;
+
+ return (isUser? <Outlet/> : <Navigate to='/login'></Navigate>)
+  
+  
+};
+export default PrivateRoute;
